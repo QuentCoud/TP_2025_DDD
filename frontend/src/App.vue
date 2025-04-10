1dcebbbb146b04e7f8c77caee54c2ccdcf7273df
Quentin Couderc <coudercq@gmail.com>
2025-04-10 15:26:36 +0200

    feat(front)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,13 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import Connexion from './views/Connexion.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      <Connexion msg="You did it!" />
     </div>
   </header>
-
-  <RouterView />
 </template>
 
 <style scoped>
