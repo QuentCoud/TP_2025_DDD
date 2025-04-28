9f675da4eb129a426d6f2b74d123bd9d2b293e37
TimotheeSvg <ssauvage.timothee@gmail.com>
2025-04-28 15:44:28 +0200

    init data

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -133,3 +134,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'main.User'
--- backend/main/signals.py
+++ backend/main/signals.py
@@ -1,11 +1,11 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import User, Artiste, Proprietaire
+from main.models import User, Artist, ConcertOwner
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         if instance.role == 'artists':
-            Artiste.objects.create(user=instance)
+            Artist.objects.create(user=instance)
         elif instance.role == 'concert_owner':
-            Proprietaire.objects.create(user=instance)
+            ConcertOwner.objects.create(user=instance)
